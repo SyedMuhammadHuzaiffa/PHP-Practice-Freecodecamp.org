169841e4854a85ec3efd0e65203f0b084b39c38d
SyedMuhammadHuzaiffa <syed.huzaiffaxD@gmail.com>
2024-04-14 00:23:11 +0500

    Initial commit

---
+++ site.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h1>Huzaifa's site </h1>";
+    echo "<hr>";
+    echo "<p>This is my site</p>";
+    ?>
+</body>
+</html>
